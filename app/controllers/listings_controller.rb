09f68418377224f208703cf9f81e4a7cf238b9c0
Gus Macfarlane <gusmac@gmail.com>
2018-08-05 10:01:30 +0800

    part done, installing devise next

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,4 +2,14 @@ class ListingsController < ApplicationController
   def new
     @listing = Listing.new
   end
+
+  def create
+    @listing = Listing.new(listing_params)
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:title, :description, :address_city, :state, :address_zip_code, :photo, :price )
+  end
 end
